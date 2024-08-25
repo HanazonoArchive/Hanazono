b86fbc8b65f8e3bbcde30b59240b0b919b3e9f99
Jay Mark Agsoy <157018362+HanazonoArchive@users.noreply.github.com>
2024-08-25 16:36:22 +0800

    Add files via upload

---
+++ js/home-js.js
@@ -0,0 +1,15 @@
+const updatesBtn = document.getElementById('updatesBtn');
+const projectsBtn = document.getElementById('projectsBtn');
+const progressBtn = document.getElementById('progressBtn');
+
+updatesBtn.addEventListener('click', () => {
+    window.location.href = 'project.html'; 
+});
+
+projectsBtn.addEventListener('click', () => {
+    window.location.href = 'project.html'; 
+});
+
+progressBtn.addEventListener('click', () => {
+    window.location.href = 'project.html'; 
+});

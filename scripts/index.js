5a503342f642ab98e3b5dc2d540a9e1215b590fd
Hanazono Archive <jaymark23x@gmail.com>
2025-01-23 15:06:18 +0800

    Update8

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+document.getElementById('home_button').addEventListener('click', function() {
+  console.log('home_button got clicked!');
+});
+
+document.getElementById('about_button').addEventListener('click', function() {
+    console.log('about_button got clicked!');
+});
+  
+document.getElementById('projects_button').addEventListener('click', function() {
+    console.log('projects_button got clicked!');
+});
+  
+document.getElementById('skills_button').addEventListener('click', function() {
+    console.log('skills_button got clicked!');
+});
+  
+document.getElementById('contact_button').addEventListener('click', function() {
+    console.log('contact_button got clicked!');
+});
+  
+        
